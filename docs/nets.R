bd177cb5e5696925dab0a3e365be0587e109b9e4
Ömer Taha Gülberk <83889951+omertahag@users.noreply.github.com>
2022-09-22 18:52:36 +0300

    c
    c

--- docs/nets.R
+++ docs/nets.R
@@ -188,7 +188,7 @@ d <- c + 17
 
 d
 
-a[, word := gsub(",", "", word, fixed = TRUE)]
+a[, word := gsub(".", "", word, fixed = TRUE)]
 
 as.numeric(a$word)
 
@@ -261,7 +261,7 @@ d <- c + c[2]-c[1]-1
 
 d
 
-a[, word := gsub(",", "", word, fixed = TRUE)]
+a[, word := gsub(".", "", word, fixed = TRUE)]
 
 as.numeric(a$word)
 
